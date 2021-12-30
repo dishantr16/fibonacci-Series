22a718c815a06d65e70fecc22086ba33678736c6
Sourcery AI <>
2021-12-30 17:38:06 +0000

    'Refactored by Sourcery'

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -2,18 +2,12 @@ for _ in range(int(input())):
     a=input()
     d={}
     for i in a:
-        if i in d:
-            d.update({i:d.get(i)+1})
-        else:
-            d.update({i:1})
-    l=[]
-    for i in d.values():
-        l.append(i)
+        d[i] = d.get(i)+1 if i in d else 1
+    l = list(d.values())
     l.sort()
     if len(l)<3:
         print("Dynamic")
+    elif l[-1]==l[-2]+l[-3]:
+        print("Dynamic")
     else:
-        if l[-1]==l[-2]+l[-3]:
-            print("Dynamic")
-        else:
-            print("Not")
+        print("Not")
